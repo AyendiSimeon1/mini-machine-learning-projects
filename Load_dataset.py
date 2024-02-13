8eb5ce3e5bfb16963706c8ef3250c8ec8f16555d
Simeon Ayendi <143404014+AyendiSimeon1@users.noreply.github.com>
2024-02-13 17:26:51 +0100

    Update Load_dataset.py

--- Load_dataset.py
+++ Load_dataset.py
@@ -1,5 +1,6 @@
-from sklearn.dataset import load_breast_cancer_dataset
+from sklearn.dataset import load_breast_cancer
 cancer = loaf_breast_cancer_dataset
 
 print("Cancer Data Keys: {}".format(cancer.keys()))
 
+print("Cancer Data shape: {}\n".format(cancer.data.shape()))
